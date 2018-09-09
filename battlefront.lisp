cec9e7c47d5326894923bcdb22dd7d755fcb1688
noffle <sww@eight45.net>
2018-09-09 10:14:04 -0700

    chore: remove unused code

--- battlefront.lisp
+++ battlefront.lisp
@@ -44,7 +44,7 @@
   ;; clear screen
   (gl:clear :color-buffer)
   ;; rotation
-  (incf *rot* 1.5)
+  (incf *rot* 0.5)
   (gl:load-identity)
   (gl:rotate *rot* 0 0 1)
   ;; quad
@@ -133,20 +133,3 @@
                      format
                      data-type
                     data)))
-
-(defun rotate (input-pathname output-pathname)
-  "Read a PNG image, rotate it 90 degrees, and write it to a new file."
-  (let* ((old (with-open-file (input input-pathname 
-                                     :element-type '(unsigned-byte 8))
-                (png:decode input)))
-         (new (png:make-image (png:image-width old)
-                              (png:image-height old)
-                              (png:image-channels old)))
-         (m (png:image-width old)))
-      (dotimes (i (png:image-height new))
-        (dotimes (j (png:image-width new))
-          (dotimes (k (png:image-channels new))
-            (setf (aref new i j k) (aref old j (- m i 1) k)))))
-      (with-open-file (output output-pathname :element-type '(unsigned-byte 8)
-                              :direction :output :if-exists :supersede)
-        (png:encode new output))))
